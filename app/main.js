a5da0f5a8fb68d4d38d0abfecbc8ce1344bbf1d5
Guy Starbuck <gstarbuck@hotmail.com>
2015-12-17 16:37:10 -0600

    Initial commit

---
+++ app/main.js
@@ -0,0 +1,54 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+///<reference path="https://trello.com/1/client.js" />
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+ if (process.platform != 'darwin')
+ app.quit();
+});
+
+app.on('ready', function(){
+	mainWindow = new BrowserWindow({width:800, height:600, icon:__dirname + '/resources/SupaStud.ico'});
+	mainWindow.loadURL('file://' + __dirname + '/index.html');
+	mainWindow.on('closed', function() {
+		mainWindow =null;
+	});
+});
+
+
+
+var authenticationSuccess = function() { console.log("Successful authentication"); };
+var authenticationFailure = function() { console.log("Failed authentication"); };
+
+function loginToTrello() {
+	
+	Trello.authorize({
+		type: "popup",
+		name: "Getting Started Application",
+		scope: {
+			read: true,
+			write: true
+		},
+		expiration: "never",
+		authenticationSuccess,
+		authenticationFailure
+	});
+	
+	
+
+}
+
+function addCard() {
+	
+	var myList = "567317217513a42e82c785f7";
+	var creationSuccess = function(data) {
+		console.log("Card created successfully.  Data returned:" + JSON.stringify(data));
+		
+	};
+	var newCard = {name: "New Test Card", desc: "This is the description of our new card.", pos: "top", idList: myList};
+	
+	Trello.post("/cards", newCard, creationSuccess);
+}
